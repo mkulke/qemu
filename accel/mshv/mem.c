a23cb00f2df07c5f5d8386d5a40cc322343aa57b
Magnus Kulke <magnuskulke@microsoft.com>
2025-02-21 15:00:22 +0100

    Removed: dirty slot tracking
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- accel/mshv/mem.c
+++ accel/mshv/mem.c
@@ -5,60 +5,13 @@
 #include <stdint.h>
 #include <sys/ioctl.h>
 
-/* #include "hw/pci/pci.h" */
-/* #include "sysemu/dma.h" */
- 
-static GHashTable *dirty_log_slots_mgns;
-static QemuMutex dirty_log_slots_mutex_mgns;
 static MemManagerMgns *mem_manager_mgns;
-static QemuMutex mem_manager_mutex_mgns;
 
 void init_mem_manager_mgns(void)
 {
-	qemu_mutex_init(&mem_manager_mutex_mgns);
 	mem_manager_mgns = g_new0(MemManagerMgns, 1);
+	qemu_mutex_init(&mem_manager_mgns->mutex);
 	mem_manager_mgns->mem_entries = NULL;
-}
-
-void init_dirty_log_slots_mgns(void)
-{
-    qemu_mutex_init(&dirty_log_slots_mutex_mgns);
-	dirty_log_slots_mgns = g_hash_table_new(g_direct_hash, g_direct_equal);
-}
-
-int set_dirty_log_slot_mgns(uint64_t guest_pfn, uint64_t memory_size)
-{
-	struct DirtyLogSlotMgns *slot;
-
-	WITH_QEMU_LOCK_GUARD(&dirty_log_slots_mutex_mgns) {
-		slot = g_new0(struct DirtyLogSlotMgns, 1);
-		if (!slot) {
-			perror("failed to allocate memory for DirtyLogSlot");
-			return -1;
-		}
-		slot->guest_pfn = guest_pfn;
-		slot->memory_size = memory_size;
-		g_hash_table_insert(dirty_log_slots_mgns, GINT_TO_POINTER(guest_pfn), slot);
-	}
-
-	return 0;
-}
-
-int remove_dirty_log_slot_mgns(uint64_t guest_pfn)
-{
-	struct DirtyLogSlotMgns *slot;
-
-	WITH_QEMU_LOCK_GUARD(&dirty_log_slots_mutex_mgns) {
-		slot = g_hash_table_lookup(dirty_log_slots_mgns, GINT_TO_POINTER(guest_pfn));
-		if (slot == NULL) {
-			perror("failed to find DirtyLogSlot entry");
-			return -1;
-		}
-		g_hash_table_remove(dirty_log_slots_mgns, GINT_TO_POINTER(guest_pfn));
-		g_free(slot);
-	}
-
-	return 0;
 }
 
 static int set_guest_memory_mgns(int vm_fd, struct mshv_user_mem_region *region)
@@ -87,13 +40,7 @@ static int create_user_memory_region_mgns(int vm_fd, struct mshv_user_mem_region
 
 	/* printf("[mgns-qemu] create user memory region: %llx\n", region->guest_pfn); */
 
-	ret = set_dirty_log_slot_mgns(region->guest_pfn, region->size);
-	if (ret < 0) {
-		perror("failed to set dirty log slot");
-		return -1;
-	}
-
-	set_guest_memory_mgns(vm_fd, region);
+	ret = set_guest_memory_mgns(vm_fd, region);
 	if (ret < 0) {
 		perror("failed to map user memory");
 		return -1;
@@ -106,12 +53,6 @@ static int remove_user_memory_region_mgns(int vm_fd, struct mshv_user_mem_region
 	int ret;
 
 	/* printf("[mgns-qemu] removing user memory region: %llx\n", region->guest_pfn); */
-
-	ret = remove_dirty_log_slot_mgns(region->guest_pfn);
-	if (ret < 0) {
-		perror("failed to remove dirty log slot");
-		return -1;
-	}
 
 	ret = set_guest_memory_mgns(vm_fd, region);
 	if (ret < 0) {
@@ -189,51 +130,6 @@ static bool find_overlap_region_mgns(size_t gpa_idx, MemoryRegionMgns *mr, size_
 	return false;
 }
 
-bool map_overlapped_region_mgns(int vm_fd, uint64_t gpa)
-{
-	size_t gpa_idx, overlap_idx;
-	MemEntryMgns *gpa_entry, *overlap_entry;
-	int ret;
-
-	WITH_QEMU_LOCK_GUARD(&mem_manager_mutex_mgns) {
-		if (!find_by_gpa_mgns(gpa, &gpa_idx)) {
-			return false;
-		}
-		gpa_entry = g_list_nth_data(mem_manager_mgns->mem_entries, gpa_idx);
-		if (!gpa_entry) {
-			perror("unexpected error. failed to find mem entry");
-			abort();
-		}
-
-		if (!find_overlap_region_mgns(gpa_idx, &gpa_entry->mr, &overlap_idx)) {
-			return false;
-		}
-		if (gpa_idx == overlap_idx) {
-			perror("unexpected error. gpa_idx == overlap_idx");
-			abort();
-		}
-
-		// unmap overlap
-		overlap_entry = g_list_nth_data(mem_manager_mgns->mem_entries, overlap_idx);
-		ret = map_or_unmap_mgns(vm_fd, &overlap_entry->mr, false);
-		if (ret < 0) {
-			perror("failed to unmap overlap region");
-			abort();
-		}
-		overlap_entry->mapped = false;
-
-		// map gpa
-		ret = map_or_unmap_mgns(vm_fd, &gpa_entry->mr, true);
-		if (ret < 0) {
-			perror("failed to map gpa region");
-			abort();
-		}
-		gpa_entry->mapped = true;
-	}
-
-	return true;
-}
-
 static inline MemEntryMgns *find_mem_entry_mgns(GList *entries,
 												const MemoryRegionMgns *mr_1)
 {
@@ -267,7 +163,7 @@ static int add_del_mem_mgns(int vm_fd, const MemoryRegionMgns *mr, bool add)
 
 	/* printf("[mgns-qemu] === add_del_mem_mgns(): =========================\n"); */
 	
-	WITH_QEMU_LOCK_GUARD(&mem_manager_mutex_mgns) {
+	WITH_QEMU_LOCK_GUARD(&mem_manager_mgns->mutex) {
 		entries = mem_manager_mgns->mem_entries;
 		entry = find_mem_entry_mgns(entries, mr);
 
@@ -313,6 +209,52 @@ static int add_del_mem_mgns(int vm_fd, const MemoryRegionMgns *mr, bool add)
 	}
 	return 0;
 }
+
+bool map_overlapped_region_mgns(int vm_fd, uint64_t gpa)
+{
+	size_t gpa_idx, overlap_idx;
+	MemEntryMgns *gpa_entry, *overlap_entry;
+	int ret;
+
+	WITH_QEMU_LOCK_GUARD(&mem_manager_mgns->mutex) {
+		if (!find_by_gpa_mgns(gpa, &gpa_idx)) {
+			return false;
+		}
+		gpa_entry = g_list_nth_data(mem_manager_mgns->mem_entries, gpa_idx);
+		if (!gpa_entry) {
+			perror("unexpected error. failed to find mem entry");
+			abort();
+		}
+
+		if (!find_overlap_region_mgns(gpa_idx, &gpa_entry->mr, &overlap_idx)) {
+			return false;
+		}
+		if (gpa_idx == overlap_idx) {
+			perror("unexpected error. gpa_idx == overlap_idx");
+			abort();
+		}
+
+		// unmap overlap
+		overlap_entry = g_list_nth_data(mem_manager_mgns->mem_entries, overlap_idx);
+		ret = map_or_unmap_mgns(vm_fd, &overlap_entry->mr, false);
+		if (ret < 0) {
+			perror("failed to unmap overlap region");
+			abort();
+		}
+		overlap_entry->mapped = false;
+
+		// map gpa
+		ret = map_or_unmap_mgns(vm_fd, &gpa_entry->mr, true);
+		if (ret < 0) {
+			perror("failed to map gpa region");
+			abort();
+		}
+		gpa_entry->mapped = true;
+	}
+
+	return true;
+}
+
 
 int add_mem_mgns(int vm_fd, const MemoryRegionMgns *mr)
 {

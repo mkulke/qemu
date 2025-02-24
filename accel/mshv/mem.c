5e5126837219492346a84fddfac718ac76f848b4
Magnus Kulke <magnuskulke@microsoft.com>
2025-02-24 15:25:44 +0100

    mem: added special case for remap userspace addr errors
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- accel/mshv/mem.c
+++ accel/mshv/mem.c
@@ -14,50 +14,45 @@ void init_mem_manager_mgns(void)
 	mem_manager_mgns->mem_entries = NULL;
 }
 
+static MemEntryMgns *find_entry_by_userspace_addr_mgns(uint64_t addr)
+{
+	MemEntryMgns *entry;
+	GList *entries;
+	size_t i = 0;
+
+	entries = mem_manager_mgns->mem_entries;
+	for(GList* elem = entries; elem != NULL; elem = elem->next) {
+		entry = elem->data;
+		/* check whether addr falls into the range of an already mapped region */
+		if (entry->mr.userspace_addr <= addr
+			&& addr - entry->mr.userspace_addr < entry->mr.memory_size
+			&& entry->mapped) {
+			return entry;
+		}
+		i++;
+	}
+
+	return NULL;
+}
+
 static int set_guest_memory_mgns(int vm_fd, struct mshv_user_mem_region *region)
 {
-	printf("[mgns-qemu] set_guest_memory_mgns (enabled)\n");
-	printf("[mgns-qemu]   guest_pfn:      0x%08llx\n", region->guest_pfn); 
-	printf("[mgns-qemu]   size:           0x%08llx\n", region->size);
-	printf("[mgns-qemu]   userspace_addr: 0x%016llx\n", region->userspace_addr);
-	printf("[mgns-qemu]   rsvd:           skip\n");
-	printf("[mgns-qemu]   flags:          0x%04x\n", region->flags);
-
 	int ret;
 
 	ret = ioctl(vm_fd, MSHV_SET_GUEST_MEMORY, region);
 	if (ret < 0) {
+		if (find_entry_by_userspace_addr_mgns(region->userspace_addr)) {
+			return -MSHV_USERSPACE_ADDR_REMAP_ERROR;
+		}
+		/* printf("[mgns-qemu] set_guest_memory_mgns\n"); */
+		/* printf("[mgns-qemu]   guest_pfn:      0x%08llx\n", region->guest_pfn); */ 
+		/* printf("[mgns-qemu]   size:           0x%08llx\n", region->size); */
+		/* printf("[mgns-qemu]   userspace_addr: 0x%016llx\n", region->userspace_addr); */
+		/* printf("[mgns-qemu]   rsvd:           skip\n"); */
+		/* printf("[mgns-qemu]   flags:          0x%04x\n", region->flags); */
+
 		perror("failed to set guest memory");
-		return errno;
-	}
-
-	return 0;
-}
-
-static int create_user_memory_region_mgns(int vm_fd, struct mshv_user_mem_region *region)
-{
-	int ret;
-
-	/* printf("[mgns-qemu] create user memory region: %llx\n", region->guest_pfn); */
-
-	ret = set_guest_memory_mgns(vm_fd, region);
-	if (ret < 0) {
-		perror("failed to map user memory");
-		return -1;
-	}
-	return 0;
-}
-
-static int remove_user_memory_region_mgns(int vm_fd, struct mshv_user_mem_region *region)
-{
-	int ret;
-
-	/* printf("[mgns-qemu] removing user memory region: %llx\n", region->guest_pfn); */
-
-	ret = set_guest_memory_mgns(vm_fd, region);
-	if (ret < 0) {
-		perror("failed to map user memory");
-		return -1;
+		return -errno;
 	}
 
 	return 0;
@@ -67,27 +62,26 @@ static int map_or_unmap_mgns(int vm_fd, const MemoryRegionMgns *mr, bool add)
 {
 	struct mshv_user_mem_region region = {0};
 
-	/* printf("[mgns-qemu] map_or_unmap_mgns: guest_phys_addr %lx, add: %u\n", mr->guest_phys_addr, add); */
-	
-	region.flags |= (1 << MSHV_SET_MEM_BIT_EXECUTABLE);
-	if (!mr->readonly) {
-		region.flags |= (1 << MSHV_SET_MEM_BIT_WRITABLE);
-	}
 	region.guest_pfn = mr->guest_phys_addr >> MSHV_PAGE_SHIFT;
 	region.size = mr->memory_size;
 	region.userspace_addr = mr->userspace_addr;
 
-	if (add) {
-		region.flags &= ~(1 << MSHV_SET_MEM_BIT_UNMAP);
-		return create_user_memory_region_mgns(vm_fd, &region);
+	/* printf("[mgns-qemu] map_or_unmap_mgns: guest_phys_addr %lx, add: %u\n", mr->guest_phys_addr, add); */
+
+	if (!add) {
+		region.flags |= (1 << MSHV_SET_MEM_BIT_UNMAP);
+		return set_guest_memory_mgns(vm_fd, &region);
 	}
 
-	region.flags = (1 << MSHV_SET_MEM_BIT_UNMAP);
-	return remove_user_memory_region_mgns(vm_fd, &region);
+	region.flags = (1 << MSHV_SET_MEM_BIT_EXECUTABLE);
+	if (!mr->readonly) {
+		region.flags |= (1 << MSHV_SET_MEM_BIT_WRITABLE);
+	}
+
+	return set_guest_memory_mgns(vm_fd, &region);
 }
 
-
-bool find_by_gpa_mgns(uint64_t addr, size_t *index)
+bool find_entry_idx_by_gpa_mgns(uint64_t addr, size_t *index)
 {
 	MemEntryMgns *entry;
 	GList *entries;
@@ -175,14 +169,22 @@ static int add_del_mem_mgns(int vm_fd, const MemoryRegionMgns *mr, bool add)
 			}
 
 			/* printf("[mgns-qemu] mem entry does not exist, adding entry and mapping\n"); */
+
 			/* add */
 			ret = map_or_unmap_mgns(vm_fd, mr, true);
-
 			entry = g_new0(MemEntryMgns, 1);
 			entry->mr = *mr;
 			/* set depending on success */
 			entry->mapped = (ret == 0);
 			mem_manager_mgns->mem_entries = g_list_append(entries, entry);
+
+			if (ret == -MSHV_USERSPACE_ADDR_REMAP_ERROR) {
+				printf("[mgns-qemu] ignoring failed remapping of userspace_addr: 0x%016lx-0x%016lx\n",
+					mr->userspace_addr,
+					mr->userspace_addr + mr->memory_size);
+				ret = 0;
+			}
+
 			return ret;
 		}
 
@@ -217,7 +219,7 @@ bool map_overlapped_region_mgns(int vm_fd, uint64_t gpa)
 	int ret;
 
 	WITH_QEMU_LOCK_GUARD(&mem_manager_mgns->mutex) {
-		if (!find_by_gpa_mgns(gpa, &gpa_idx)) {
+		if (!find_entry_idx_by_gpa_mgns(gpa, &gpa_idx)) {
 			return false;
 		}
 		gpa_entry = g_list_nth_data(mem_manager_mgns->mem_entries, gpa_idx);

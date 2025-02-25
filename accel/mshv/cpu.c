53a3ec83a6448958f3728eb8d7abff9fb3e52478
Magnus Kulke <magnuskulke@microsoft.com>
2025-02-25 14:57:15 +0100

    cpu: implement get_vcpu (registers)
    Signed-off-by: Magnus Kulke <magnuskulke@microsoft.com>

--- accel/mshv/cpu.c
+++ accel/mshv/cpu.c
@@ -41,7 +41,7 @@ void remove_vcpu_mgns(int vcpu_fd)
 int new_vcpu_mgns(int mshv_fd, uint8_t vp_index, MshvOps *ops)
 {
 	int ret, vcpu_fd;
-	
+
 	ret = create_vcpu_mgns(mshv_fd, vp_index);
 	if (ret < 0) {
 		return ret;
@@ -59,3 +59,321 @@ int new_vcpu_mgns(int mshv_fd, uint8_t vp_index, MshvOps *ops)
 
 	return 0;
 }
+
+static enum hv_register_name STANDARD_REGISTER_NAMES[18] = {
+	HV_X64_REGISTER_RAX,
+	HV_X64_REGISTER_RBX,
+	HV_X64_REGISTER_RCX,
+	HV_X64_REGISTER_RDX,
+	HV_X64_REGISTER_RSI,
+	HV_X64_REGISTER_RDI,
+	HV_X64_REGISTER_RSP,
+	HV_X64_REGISTER_RBP,
+	HV_X64_REGISTER_R8,
+	HV_X64_REGISTER_R9,
+	HV_X64_REGISTER_R10,
+	HV_X64_REGISTER_R11,
+	HV_X64_REGISTER_R12,
+	HV_X64_REGISTER_R13,
+	HV_X64_REGISTER_R14,
+	HV_X64_REGISTER_R15,
+	HV_X64_REGISTER_RIP,
+	HV_X64_REGISTER_RFLAGS,
+};
+
+static enum hv_register_name SPECIAL_REGISTER_NAMES[18] = {
+	HV_X64_REGISTER_CS,
+	HV_X64_REGISTER_DS,
+	HV_X64_REGISTER_ES,
+	HV_X64_REGISTER_FS,
+	HV_X64_REGISTER_GS,
+	HV_X64_REGISTER_SS,
+	HV_X64_REGISTER_TR,
+	HV_X64_REGISTER_LDTR,
+	HV_X64_REGISTER_GDTR,
+	HV_X64_REGISTER_IDTR,
+	HV_X64_REGISTER_CR0,
+	HV_X64_REGISTER_CR2,
+	HV_X64_REGISTER_CR3,
+	HV_X64_REGISTER_CR4,
+	HV_X64_REGISTER_CR8,
+	HV_X64_REGISTER_EFER,
+	HV_X64_REGISTER_APIC_BASE,
+};
+
+static enum hv_register_name FPU_REGISTER_NAMES[26] = {
+	HV_X64_REGISTER_XMM0,
+	HV_X64_REGISTER_XMM1,
+	HV_X64_REGISTER_XMM2,
+	HV_X64_REGISTER_XMM3,
+	HV_X64_REGISTER_XMM4,
+	HV_X64_REGISTER_XMM5,
+	HV_X64_REGISTER_XMM6,
+	HV_X64_REGISTER_XMM7,
+	HV_X64_REGISTER_XMM8,
+	HV_X64_REGISTER_XMM9,
+	HV_X64_REGISTER_XMM10,
+	HV_X64_REGISTER_XMM11,
+	HV_X64_REGISTER_XMM12,
+	HV_X64_REGISTER_XMM13,
+	HV_X64_REGISTER_XMM14,
+	HV_X64_REGISTER_XMM15,
+	HV_X64_REGISTER_FP_MMX0,
+	HV_X64_REGISTER_FP_MMX1,
+	HV_X64_REGISTER_FP_MMX2,
+	HV_X64_REGISTER_FP_MMX3,
+	HV_X64_REGISTER_FP_MMX4,
+	HV_X64_REGISTER_FP_MMX5,
+	HV_X64_REGISTER_FP_MMX6,
+	HV_X64_REGISTER_FP_MMX7,
+	HV_X64_REGISTER_FP_CONTROL_STATUS,
+	HV_X64_REGISTER_XMM_CONTROL_STATUS,
+};
+
+inline static int get_generic_regs_mgns(int cpu_fd,
+										struct hv_register_assoc *assocs,
+										size_t n_regs)
+{
+	struct mshv_vp_registers input = {
+		.count = n_regs,
+		.regs = assocs,
+	};
+
+	return ioctl(cpu_fd, MSHV_GET_VP_REGISTERS, &input);
+}
+
+inline static void populate_standard_regs_mgns(struct hv_register_assoc *assocs,
+										       struct StandardRegisters *regs)
+{
+	regs->rax =	assocs[0].value.reg64;
+	regs->rbx =	assocs[1].value.reg64;
+	regs->rcx =	assocs[2].value.reg64;
+	regs->rdx =	assocs[3].value.reg64;
+	regs->rsi =	assocs[4].value.reg64;
+	regs->rdi =	assocs[5].value.reg64;
+	regs->rsp =	assocs[6].value.reg64;
+	regs->rbp =	assocs[7].value.reg64;
+	regs->r8  =	assocs[8].value.reg64;
+	regs->r9  =	assocs[9].value.reg64;
+	regs->r10 =	assocs[10].value.reg64;
+	regs->r11 =	assocs[11].value.reg64;
+	regs->r12 =	assocs[12].value.reg64;
+	regs->r13 =	assocs[13].value.reg64;
+	regs->r14 =	assocs[14].value.reg64;
+	regs->r15 =	assocs[15].value.reg64;
+	regs->rip =	assocs[16].value.reg64;
+
+	regs->rflags = assocs[17].value.reg64;
+}
+
+inline static void populate_segment_reg_mgns(struct hv_x64_segment_register *hv_seg,
+											 struct SegmentRegister *seg)
+{
+	*seg = (struct SegmentRegister){0};
+
+	seg->base = hv_seg->base;
+	seg->limit = hv_seg->limit;
+	seg->selector = hv_seg->selector;
+	seg->unusable = 0;
+	seg->padding = 0;
+}
+
+inline static void populate_table_reg_mgns(struct hv_x64_segment_register *hv_seg,
+										   struct TableRegister *seg)
+{
+	*seg = (struct TableRegister){0};
+
+	seg->base = hv_seg->base;
+	seg->limit = hv_seg->limit;
+}
+
+inline static void populate_interrupt_bitmap_mgns(uint64_t pending_reg, uint64_t *bitmap)
+{
+	uint64_t interrupt_nr;
+	/* TODO: early exit */
+	/* Check if the least significant bit is 1 (interruption pending) */
+	/* and that the trailing zero count (after shifting right by 1) is >= 3. */
+	if (((pending_reg & 0x01ULL) == 1ULL)
+		&& (__builtin_ctzll(pending_reg >> 1) >= 3)) {
+		/* Extract the interrupt number from bits 16 and above. */
+        interrupt_nr = pending_reg >> 16;
+        if (interrupt_nr > 255) {
+			perror("invalid interrupt vector number > 255");
+            abort();
+        }
+
+        /* Compute the bit offset (lower 6 bits, i.e. 0-63) */
+        uint64_t bit_offset = pending_reg & 0x3FULL;
+        /* The index is stored in the remaining higher bits (shift right by 6) */
+        uint64_t index = pending_reg >> 6;
+
+        /* Set the corresponding bit in the interrupt bitmap. */
+        /* (63 - bit_offset) shifts from the left. */
+        bitmap[index] = 1ULL << (63 - bit_offset);
+	}
+}
+
+inline static void populate_special_regs_mgns(struct hv_register_assoc *assocs,
+										      struct SpecialRegisters *regs)
+{
+	uint64_t pending_reg;
+
+	populate_segment_reg_mgns(&assocs[0].value.segment, &regs->cs);
+	populate_segment_reg_mgns(&assocs[1].value.segment, &regs->ds);
+	populate_segment_reg_mgns(&assocs[2].value.segment, &regs->es);
+	populate_segment_reg_mgns(&assocs[3].value.segment, &regs->fs);
+	populate_segment_reg_mgns(&assocs[4].value.segment, &regs->gs);
+	populate_segment_reg_mgns(&assocs[5].value.segment, &regs->ss);
+	populate_segment_reg_mgns(&assocs[6].value.segment, &regs->tr);
+	populate_segment_reg_mgns(&assocs[7].value.segment, &regs->ldt);
+
+	populate_table_reg_mgns(&assocs[8].value.segment, &regs->gdt);
+	populate_table_reg_mgns(&assocs[9].value.segment, &regs->idt);
+
+	regs->cr0	  	= assocs[10].value.reg64;
+	regs->cr2	  	= assocs[11].value.reg64;
+	regs->cr3	  	= assocs[12].value.reg64;
+	regs->cr4	  	= assocs[13].value.reg64;
+	regs->cr8	  	= assocs[14].value.reg64;
+	regs->efer 		= assocs[15].value.reg64;
+	regs->apic_base = assocs[16].value.reg64;
+
+	pending_reg = assocs[17].value.pending_interruption.as_uint64;
+	populate_interrupt_bitmap_mgns(pending_reg, regs->interrupt_bitmap);
+}
+
+static int get_standard_regs_mgns(int cpu_fd, struct StandardRegisters *regs)
+{
+	size_t n_regs = sizeof(STANDARD_REGISTER_NAMES) / sizeof(enum hv_register_name);
+	struct hv_register_assoc *assocs;
+	int ret;
+
+	// TODO: maybe make this global?
+	assocs = g_new0(struct hv_register_assoc, n_regs);
+	for (size_t i = 0; i < n_regs; i++) {
+		assocs[i].name = STANDARD_REGISTER_NAMES[i];
+	}
+	ret = get_generic_regs_mgns(cpu_fd, assocs, n_regs);
+	if (ret < 0) {
+		perror("failed to get standard registers");
+		g_free(assocs);
+		return -errno;
+	}
+
+	populate_standard_regs_mgns(assocs, regs);
+	g_free(assocs);
+	return 0;
+}
+
+static int get_special_regs_mgns(int cpu_fd, struct SpecialRegisters *regs)
+{
+	size_t n_regs = sizeof(SPECIAL_REGISTER_NAMES) / sizeof(enum hv_register_name);
+	struct hv_register_assoc *assocs;
+	int ret;
+
+	assocs = g_new0(struct hv_register_assoc, n_regs);
+	for (size_t i = 0; i < n_regs; i++) {
+		assocs[i].name = SPECIAL_REGISTER_NAMES[i];
+	}
+	ret = get_generic_regs_mgns(cpu_fd, assocs, n_regs);
+	if (ret < 0) {
+		perror("failed to get special registers");
+		g_free(assocs);
+		return -errno;
+	}
+	populate_special_regs_mgns(assocs, regs);
+	g_free(assocs);
+	return 0;
+}
+
+inline static void populate_fpu_regs_mgns(struct hv_register_assoc *assocs,
+										  struct FloatingPointUnit *fpu)
+{
+	union hv_x64_fp_control_status_register fp_control_status;
+	union hv_x64_xmm_control_status_register xmm_control_status;
+	struct hv_register_assoc *assoc;
+	uint8_t (*xmm)[16];
+	size_t fp_i;
+	uint8_t (*fpr)[16];
+
+	fp_control_status = assocs[24].value.fp_control_status;
+	xmm_control_status = assocs[25].value.xmm_control_status;
+
+	memset(fpu->fpr, 0, sizeof(fpu->fpr));
+	fpu->fcw = fp_control_status.fp_control;
+	fpu->fsw = fp_control_status.fp_status;
+	fpu->ftwx = fp_control_status.fp_tag;
+	fpu->pad1 = 0;
+	fpu->last_opcode = fp_control_status.last_fp_op;
+	fpu->last_ip = fp_control_status.last_fp_rip;
+	fpu->last_dp = xmm_control_status.last_fp_rdp;
+	memset(fpu->xmm, 0, sizeof(fpu->xmm));
+	fpu->mxcsr = xmm_control_status.xmm_status_control;
+	fpu->pad2 = 0;
+
+	/* First 16 registers are xmm0-xmm15 */
+	for (size_t i = 0; i < 16; i++) {
+		assoc = &assocs[i];
+		/* Copy 16 bytes from assoc to xmm[i] */
+		xmm = &fpu->xmm[i];
+		memcpy(xmm, &assoc->value.reg128, 16);
+	}
+
+	/* Next 8 registers are fp_mmx0-fp_mmx7 */
+	for (size_t i = 16; i < 24; i++) {
+		assoc = &assocs[i];
+		fp_i = (i - 16);
+		/* Copy 16 bytes from assoc to fpr[fp_i] */
+		fpr = &fpu->fpr[fp_i];
+		memcpy(fpr, &assoc->value.reg128, 16);
+	}
+}
+
+static int get_fpu_regs_mgns(int cpu_fd, struct FloatingPointUnit *regs)
+{
+	size_t n_regs = sizeof(FPU_REGISTER_NAMES) / sizeof(enum hv_register_name);
+	struct hv_register_assoc *assocs;
+	int ret;
+
+	assocs = g_new0(struct hv_register_assoc, n_regs);
+	for (size_t i = 0; i < n_regs; i++) {
+		assocs[i].name = FPU_REGISTER_NAMES[i];
+	}
+	ret = get_generic_regs_mgns(cpu_fd, assocs, n_regs);
+	if (ret < 0) {
+		perror("failed to get fpu registers");
+		g_free(assocs);
+		return -errno;
+	}
+	populate_fpu_regs_mgns(assocs, regs);
+	g_free(assocs);
+	return 0;
+}
+
+int get_vcpu_mgns(int cpu_fd,
+                  struct StandardRegisters *standard_regs,
+                  struct SpecialRegisters *special_regs,
+                  struct FloatingPointUnit *fpu_regs)
+{
+	int ret;
+
+	ret = get_standard_regs_mgns(cpu_fd, standard_regs);
+	if (ret < 0) {
+		return ret;
+	}
+	ret = get_special_regs_mgns(cpu_fd, special_regs);
+	if (ret < 0) {
+		return ret;
+	}
+	ret = get_fpu_regs_mgns(cpu_fd, fpu_regs);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return 0;
+}
+/* int get_reg_mgns(int vcpu_fd, struct hv_register_assoc *reg_assocs, size_t n_regs) */
+/* int get_regs_mgns(int vcpu_fd) */
+/* { */
+/* 	return 0; */
+/* } */
--- include/hw/hyperv/linux-mshv.h
+++ include/hw/hyperv/linux-mshv.h
@@ -21,6 +21,303 @@
 
 #define MSHV_IOCTL	0xB8
 
+enum hv_register_name {
+	/* X64 User-Mode Registers */
+	HV_X64_REGISTER_RAX		= 0x00020000,
+	HV_X64_REGISTER_RCX		= 0x00020001,
+	HV_X64_REGISTER_RDX		= 0x00020002,
+	HV_X64_REGISTER_RBX		= 0x00020003,
+	HV_X64_REGISTER_RSP		= 0x00020004,
+	HV_X64_REGISTER_RBP		= 0x00020005,
+	HV_X64_REGISTER_RSI		= 0x00020006,
+	HV_X64_REGISTER_RDI		= 0x00020007,
+	HV_X64_REGISTER_R8		= 0x00020008,
+	HV_X64_REGISTER_R9		= 0x00020009,
+	HV_X64_REGISTER_R10		= 0x0002000A,
+	HV_X64_REGISTER_R11		= 0x0002000B,
+	HV_X64_REGISTER_R12		= 0x0002000C,
+	HV_X64_REGISTER_R13		= 0x0002000D,
+	HV_X64_REGISTER_R14		= 0x0002000E,
+	HV_X64_REGISTER_R15		= 0x0002000F,
+	HV_X64_REGISTER_RIP		= 0x00020010,
+	HV_X64_REGISTER_RFLAGS	= 0x00020011,
+
+	/* X64 Floating Point and Vector Registers */
+	HV_X64_REGISTER_XMM0				= 0x00030000,
+	HV_X64_REGISTER_XMM1				= 0x00030001,
+	HV_X64_REGISTER_XMM2				= 0x00030002,
+	HV_X64_REGISTER_XMM3				= 0x00030003,
+	HV_X64_REGISTER_XMM4				= 0x00030004,
+	HV_X64_REGISTER_XMM5				= 0x00030005,
+	HV_X64_REGISTER_XMM6				= 0x00030006,
+	HV_X64_REGISTER_XMM7				= 0x00030007,
+	HV_X64_REGISTER_XMM8				= 0x00030008,
+	HV_X64_REGISTER_XMM9				= 0x00030009,
+	HV_X64_REGISTER_XMM10				= 0x0003000A,
+	HV_X64_REGISTER_XMM11				= 0x0003000B,
+	HV_X64_REGISTER_XMM12				= 0x0003000C,
+	HV_X64_REGISTER_XMM13				= 0x0003000D,
+	HV_X64_REGISTER_XMM14				= 0x0003000E,
+	HV_X64_REGISTER_XMM15				= 0x0003000F,
+	HV_X64_REGISTER_FP_MMX0				= 0x00030010,
+	HV_X64_REGISTER_FP_MMX1				= 0x00030011,
+	HV_X64_REGISTER_FP_MMX2				= 0x00030012,
+	HV_X64_REGISTER_FP_MMX3				= 0x00030013,
+	HV_X64_REGISTER_FP_MMX4				= 0x00030014,
+	HV_X64_REGISTER_FP_MMX5				= 0x00030015,
+	HV_X64_REGISTER_FP_MMX6				= 0x00030016,
+	HV_X64_REGISTER_FP_MMX7				= 0x00030017,
+	HV_X64_REGISTER_FP_CONTROL_STATUS	= 0x00030018,
+	HV_X64_REGISTER_XMM_CONTROL_STATUS	= 0x00030019,
+
+	/* X64 Control Registers */
+	HV_X64_REGISTER_CR0	= 0x00040000,
+	HV_X64_REGISTER_CR2	= 0x00040001,
+	HV_X64_REGISTER_CR3	= 0x00040002,
+	HV_X64_REGISTER_CR4	= 0x00040003,
+	HV_X64_REGISTER_CR8	= 0x00040004,
+
+	/* X64 Segment Registers */
+	HV_X64_REGISTER_ES		= 0x00060000,
+	HV_X64_REGISTER_CS		= 0x00060001,
+	HV_X64_REGISTER_SS		= 0x00060002,
+	HV_X64_REGISTER_DS		= 0x00060003,
+	HV_X64_REGISTER_FS		= 0x00060004,
+	HV_X64_REGISTER_GS		= 0x00060005,
+	HV_X64_REGISTER_LDTR	= 0x00060006,
+	HV_X64_REGISTER_TR		= 0x00060007,
+
+	/* X64 Table Registers */
+	HV_X64_REGISTER_IDTR	= 0x00070000,
+	HV_X64_REGISTER_GDTR	= 0x00070001,
+
+	/* X64 Virtualized MSRs */
+	HV_X64_REGISTER_EFER		= 0x00080001,
+	HV_X64_REGISTER_APIC_BASE	= 0x00080003,
+};
+
+struct hv_u128 {
+	__u64 low_part;
+	__u64 high_part;
+};
+
+union hv_x64_xmm_control_status_register {
+	struct hv_u128 as_uint128;
+	struct {
+		union {
+			/* long mode */
+			__u64 last_fp_rdp;
+			/* 32 bit mode */
+			struct {
+				__u32 last_fp_dp;
+				__u16 last_fp_ds;
+				__u16 padding;
+			};
+		};
+		__u32 xmm_status_control;
+		__u32 xmm_status_control_mask;
+	};
+};
+
+union hv_x64_fp_register {
+	struct hv_u128 as_uint128;
+	struct {
+		__u64 mantissa;
+		__u64 biased_exponent : 15;
+		__u64 sign : 1;
+		__u64 reserved : 48;
+	};
+};
+
+union hv_x64_pending_exception_event {
+	__u64 as_uint64[2];
+	struct {
+		__u32 event_pending : 1;
+		__u32 event_type : 3;
+		__u32 reserved0 : 4;
+		__u32 deliver_error_code : 1;
+		__u32 reserved1 : 7;
+		__u32 vector : 16;
+		__u32 error_code;
+		__u64 exception_parameter;
+	};
+};
+
+union hv_x64_pending_virtualization_fault_event {
+	__u64 as_uint64[2];
+	struct {
+		__u32 event_pending : 1;
+		__u32 event_type : 3;
+		__u32 reserved0 : 4;
+		__u32 reserved1 : 8;
+		__u32 parameter0 : 16;
+		__u32 code;
+		__u64 parameter1;
+	};
+};
+
+union hv_x64_pending_interruption_register {
+	__u64 as_uint64;
+	struct {
+		__u32 interruption_pending : 1;
+		__u32 interruption_type : 3;
+		__u32 deliver_error_code : 1;
+		__u32 instruction_length : 4;
+		__u32 nested_event : 1;
+		__u32 reserved : 6;
+		__u32 interruption_vector : 16;
+		__u32 error_code;
+	};
+};
+
+union hv_x64_register_sev_control {
+	__u64 as_uint64;
+	struct {
+		__u64 enable_encrypted_state : 1;
+		__u64 reserved_z : 11;
+		__u64 vmsa_gpa_page_number : 52;
+	};
+};
+
+union hv_x64_msr_npiep_config_contents {
+	__u64 as_uint64;
+	struct {
+		/*
+		 * These bits enable instruction execution prevention for
+		 * specific instructions.
+		 */
+		__u64 prevents_gdt : 1;
+		__u64 prevents_idt : 1;
+		__u64 prevents_ldt : 1;
+		__u64 prevents_tr : 1;
+
+		/* The reserved bits must always be 0. */
+		__u64 reserved : 60;
+	};
+};
+
+struct hv_x64_segment_register {
+	__u64 base;
+	__u32 limit;
+	__u16 selector;
+	union {
+		struct {
+			__u16 segment_type : 4;
+			__u16 non_system_segment : 1;
+			__u16 descriptor_privilege_level : 2;
+			__u16 present : 1;
+			__u16 reserved : 4;
+			__u16 available : 1;
+			__u16 _long : 1;
+			__u16 _default : 1;
+			__u16 granularity : 1;
+		};
+		__u16 attributes;
+	};
+};
+
+struct hv_x64_table_register {
+	__u16 pad[3];
+	__u16 limit;
+	__u64 base;
+};
+
+union hv_x64_fp_control_status_register {
+	struct hv_u128 as_uint128;
+	struct {
+		__u16 fp_control;
+		__u16 fp_status;
+		__u8 fp_tag;
+		__u8 reserved;
+		__u16 last_fp_op;
+		union {
+			/* long mode */
+			__u64 last_fp_rip;
+			/* 32 bit mode */
+			struct {
+				__u32 last_fp_eip;
+				__u16 last_fp_cs;
+				__u16 padding;
+			};
+		};
+	};
+};
+
+/* General Hypervisor Register Content Definitions */
+
+union hv_explicit_suspend_register {
+	__u64 as_uint64;
+	struct {
+		__u64 suspended : 1;
+		__u64 reserved : 63;
+	};
+};
+
+union hv_internal_activity_register {
+	__u64 as_uint64;
+
+	struct {
+		__u64 startup_suspend : 1;
+		__u64 halt_suspend : 1;
+		__u64 idle_suspend : 1;
+		__u64 rsvd_z : 61;
+	};
+};
+
+union hv_x64_interrupt_state_register {
+	__u64 as_uint64;
+	struct {
+		__u64 interrupt_shadow : 1;
+		__u64 nmi_masked : 1;
+		__u64 reserved : 62;
+	};
+};
+
+union hv_intercept_suspend_register {
+	__u64 as_uint64;
+	struct {
+		__u64 suspended : 1;
+		__u64 reserved : 63;
+	};
+};
+
+union hv_register_value {
+	struct hv_u128 reg128;
+	__u64 reg64;
+	__u32 reg32;
+	__u16 reg16;
+	__u8 reg8;
+	union hv_x64_fp_register fp;
+	union hv_x64_fp_control_status_register fp_control_status;
+	union hv_x64_xmm_control_status_register xmm_control_status;
+	struct hv_x64_segment_register segment;
+	struct hv_x64_table_register table;
+	union hv_explicit_suspend_register explicit_suspend;
+	union hv_intercept_suspend_register intercept_suspend;
+	union hv_internal_activity_register internal_activity;
+	union hv_x64_interrupt_state_register interrupt_state;
+	union hv_x64_pending_interruption_register pending_interruption;
+	union hv_x64_msr_npiep_config_contents npiep_config;
+	union hv_x64_pending_exception_event pending_exception_event;
+	union hv_x64_pending_virtualization_fault_event
+		pending_virtualization_fault_event;
+	union hv_x64_register_sev_control sev_control;
+};
+
+struct hv_register_assoc {
+	__u32 name;			/* enum hv_register_name */
+	__u32 reserved1;
+	__u64 reserved2;
+	union hv_register_value value;
+};
+
+#define MSHV_VP_MAX_REGISTERS	128
+
+struct mshv_vp_registers {
+	int count; /* at most MSHV_VP_MAX_REGISTERS */
+	struct hv_register_assoc *regs;
+};
+
 /**
  * struct mshv_user_mem_region - arguments for MSHV_SET_GUEST_MEMORY
  * @size: Size of the memory region (bytes). Must be aligned to PAGE_SIZE
@@ -159,6 +456,9 @@ struct mshv_create_vp {
 #define MSHV_IRQFD					_IOW(MSHV_IOCTL, 0x03, struct mshv_user_irqfd)
 #define MSHV_IOEVENTFD			    _IOW(MSHV_IOCTL, 0x04, struct mshv_user_ioeventfd)
 #define MSHV_SET_MSI_ROUTING		_IOW(MSHV_IOCTL, 0x05, struct mshv_user_irq_table)
+
+/* TODO: replace with ROOT_HVCALL */
+#define MSHV_GET_VP_REGISTERS		_IOWR(MSHV_IOCTL, 0xF0, struct mshv_vp_registers)
 
 /**
  * struct mshv_root_hvcall - arguments for MSHV_ROOT_HVCALL

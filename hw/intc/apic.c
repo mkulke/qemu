a5703e00059d4a175ff6445d9d5de8fabc810f24
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-04-10 11:32:33 +0200

    cosmetic: renamings in interrupt subsystem
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>

--- hw/intc/apic.c
+++ hw/intc/apic.c
@@ -936,7 +936,8 @@ static void apic_send_msi(MSIMessage *msi)
 #ifdef CONFIG_MSHV
     if (mshv_enabled()) {
 		/* TODO: error handling? */
-        request_interrupt_mgns(mshv_state->vm, delivery, vector, dest, dest_mode, trigger_mode);
+        mshv_request_interrupt(mshv_state->vm, delivery, vector, dest,
+                               dest_mode, trigger_mode);
         return;
     }
 #endif

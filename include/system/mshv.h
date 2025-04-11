c63c39d7fa66dc254ea49a7f9fd55cc5bcfbcd08
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-04-11 12:08:27 +0200

    chore: add copyright note
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>

--- include/system/mshv.h
+++ include/system/mshv.h
@@ -1,3 +1,17 @@
+/*
+ * QEMU MSHV support
+ *
+ * Copyright Microsoft, Corp. 2025
+ *
+ * Authors:
+ *  Ziqiao Zhou       <ziqiaozhou@microsoft.com>
+ *  Magnus Kulke      <magnuskulke@microsoft.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
 #ifndef QEMU_MSHV_INT_H
 #define QEMU_MSHV_INT_H
 

f9674e043fe11b22a8a090fea65d97d3b938000e
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-05-14 15:09:40 +0200

    target/i386/mshv: Add x86 decoder/emu implementation
    The MSHV accelerator requires a x86 decoder/emulator in userland to
    emulate MMIO instructions. This change contains the implementations for
    the generalized i386 instruction decoder/emulator.
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>

---
+++ include/system/mshv.h
@@ -0,0 +1,30 @@
+/*
+ * QEMU MSHV support
+ *
+ * Copyright Microsoft, Corp. 2025
+ *
+ * Authors: Ziqiao Zhou  <ziqiaozhou@microsoft.com>
+ *          Magnus Kulke <magnuskulke@microsoft.com>
+ *          Jinank Jain  <jinankjain@microsoft.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+#ifndef QEMU_MSHV_INT_H
+#define QEMU_MSHV_INT_H
+
+#ifdef COMPILING_PER_TARGET
+#ifdef CONFIG_MSHV
+#define CONFIG_MSHV_IS_POSSIBLE
+#endif
+#else
+#define CONFIG_MSHV_IS_POSSIBLE
+#endif
+
+/* cpu */
+/* EFER (technically not a register) bits */
+#define EFER_LMA   ((uint64_t)0x400)
+#define EFER_LME   ((uint64_t)0x100)
+
+#endif

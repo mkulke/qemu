744c98b6695c687f99d4c0761730e6b4ee0f661a
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-05-14 15:36:56 +0200

    hw/intc/apic: generalized fn names kvm_* => accel_*
    The mshv accelerator also provides an APIC, so we can reuse the existing
    infra from KVM, hence we rename the functions to represent generalized
    accelerator architectures.
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>

--- include/system/mshv.h
+++ include/system/mshv.h
@@ -24,9 +24,31 @@
 #define CONFIG_MSHV_IS_POSSIBLE
 #endif
 
+
+#ifdef CONFIG_MSHV_IS_POSSIBLE
+extern bool mshv_allowed;
+#define mshv_enabled() (mshv_allowed)
+#else /* CONFIG_MSHV_IS_POSSIBLE */
+#define mshv_enabled() false
+#endif
+#ifdef MSHV_USE_KERNEL_GSI_IRQFD
+#define mshv_msi_via_irqfd_enabled() mshv_enabled()
+#else
+#define mshv_msi_via_irqfd_enabled() false
+#endif
+
 /* cpu */
 /* EFER (technically not a register) bits */
 #define EFER_LMA   ((uint64_t)0x400)
 #define EFER_LME   ((uint64_t)0x100)
+
+/* interrupt */
+int mshv_irqchip_add_msi_route(int vector, PCIDevice *dev);
+int mshv_irqchip_update_msi_route(int virq, MSIMessage msg, PCIDevice *dev);
+void mshv_irqchip_commit_routes(void);
+void mshv_irqchip_release_virq(int virq);
+int mshv_irqchip_add_irqfd_notifier_gsi(const EventNotifier *n,
+                                        const EventNotifier *rn, int virq);
+int mshv_irqchip_remove_irqfd_notifier_gsi(const EventNotifier *n, int virq);
 
 #endif

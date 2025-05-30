e39d649b47a00107839b83534290301ff3f62946
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-05-30 16:22:16 +0200

    accel/mshv: Add basic interrupt injection support
    Implement initial interrupt handling logic in the MSHV backend. This
    includes management of MSI and un/registering of irqfd mechanisms.
    
    Co-authored-by: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>

--- accel/mshv/mshv-all.c
+++ accel/mshv/mshv-all.c
@@ -352,10 +352,8 @@ static MemoryListener mshv_memory_listener = {
     .priority = MEMORY_LISTENER_PRIORITY_ACCEL,
     .region_add = mem_region_add,
     .region_del = mem_region_del,
-#ifdef MSHV_USE_IOEVENTFD
     .eventfd_add = mem_ioeventfd_add,
     .eventfd_del = mem_ioeventfd_del,
-#endif
 };
 
 static MemoryListener mshv_io_listener = {
@@ -414,6 +412,8 @@ static int mshv_init(MachineState *ms)
     if (ret < 0) {
         return -1;
     }
+
+    mshv_init_msicontrol();
 
     create_vm(mshv_fd, &vm_fd);
 
